01c218343c521eccc47036a9a1c6699c50be518b
trvndng <trvndng@gmail.com>
2024-10-24 17:15:35 +0700

    Create quizlet.snippet#1.js

---
+++ quizlet.snippet#1.js
@@ -0,0 +1,34 @@
+// Hàm tải xuống tệp CSV
+function downloadCSV(csvContent) {
+    const csvFile = new Blob([csvContent],{
+        type: 'text/csv'
+    });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(csvFile);
+    link.download = document.title + '.csv';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+// Lấy tất cả các phần tử chứa thuật ngữ
+let csv = [];
+let terms = document.querySelectorAll(".SetPageTerms-term");
+
+// Lặp qua các phần tử thuật ngữ để thu thập dữ liệu
+terms.forEach( (term) => {
+    let enElement = term.querySelector("span.TermText.notranslate.lang-en");
+    let viElement = term.querySelector("span.TermText.notranslate.lang-vi");
+    let imgElement = term.querySelector("img.SetPageTerm-image");
+
+    let en = enElement ? enElement.innerHTML : "N/A";
+    let vi = viElement ? viElement.innerHTML : "N/A";
+    let imgSrc = imgElement ? imgElement.src.split('/').pop() : null;
+
+    // Thêm vào mảng CSV
+    csv.push(`${en};${vi};${imgSrc};`);
+}
+);
+
+//console.log(csv.join('\n'));
+downloadCSV(csv.join('\n'));
